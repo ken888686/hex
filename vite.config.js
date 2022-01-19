6b20442bdee495a6c903853cd6312c587cb23854
G6520 <g6520@evertrust.com.tw>
2022-01-19 10:08:17 +0800

    vite configuration

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/hex/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@/*': path.resolve(__dirname, './src/*'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
