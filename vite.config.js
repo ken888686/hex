19a7ed04200dd771f34ee75f9f49fd46e65abb73
Aaron Tu <ken888686@gmail.com>
2022-01-19 21:56:28 +0800

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hex/',
+  base: '/vite-vue3-app/',
   plugins: [vue()],
   resolve: {
     alias: {
